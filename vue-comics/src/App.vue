0a6bb465e34511f2fa872bc1683c85de172f154e
maurizio-marotta <mauriziomarotta@live.it>
2023-05-04 17:18:18 +0200

    inizio

---
+++ vue-comics/src/App.vue
@@ -0,0 +1,16 @@
+
+<script>
+export default {
+  name: 'display'
+}
+</script>
+
+<template>
+  <div class="container">
+    <h1>ciao</h1>
+  </div>
+</template>
+
+<style lang="">
+
+</style>
---
+++ vue-comics/src/components/header.vue
@@ -0,0 +1,14 @@
+
+<script>
+export default {
+  name: 'header'
+}
+</script>
+
+<template>
+  
+</template>
+
+<style>
+
+</style>
