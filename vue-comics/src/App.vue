cbb03433ad22d46bcfdeca9d67c6292949c07cfd
maurizio-marotta <mauriziomarotta@live.it>
2023-05-04 18:50:26 +0200

    main finito inzio footer

--- vue-comics/src/App.vue
+++ vue-comics/src/App.vue
@@ -15,7 +15,7 @@ export default {
 </script>
 
 <template>
-  <div>
+  <div class="">
     <headerVue />
     <mainVue />
     <footerVue />
--- vue-comics/src/components/main.vue
+++ vue-comics/src/components/main.vue
@@ -6,11 +6,21 @@ export default {
 </script>
 
 <template>
-  <div>
-    main
+  <div class="root-2">
+    <h1> &rarr;content goes here&larr; </h1>
   </div>
 </template>
 
 <style>
-
+.root-2{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100px;
+  background-color: #1C1C1C;
+}
+h1{
+  color: white;
+}
 </style>
